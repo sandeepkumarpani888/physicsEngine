326912460965a0b27d470bd75caa449b714db6be
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-01-10 19:00:02 +0530

    Movement class basic skeleton.

---
+++ movement.hpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+class Movement
+{
+
+private:
+	std::tuple<int,int> pos;
+	std::tuple<double,double> velocity;
+
+public:
+	Movement(){
+		this->pos=std::tuple<int,int>(100,0);
+		this->velocity=std::tuple<double,double>(0,0);
+	};
+	
+	Movement(std::tuple<int,int> _pos,std::tuple<double,double> _vel){
+		this->pos=_pos;
+		this->velocity=_vel;
+	};
+
+	std::tuple<int,int> getCordinates(){
+		return this->pos;
+	};
+
+	std::tuple<double,double> getVelocity(){
+		return this->velocity;
+	};
+};
