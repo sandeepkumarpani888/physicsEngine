588e70ac2a05875c09d318feaf91cd1903bfd96d
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-01-10 19:00:34 +0530

    Particle class basic skeleton.

---
+++ particle.hpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include "movement.hpp"
+
+class Particle
+{
+
+private:
+	Movement move;
+	int mass;
+
+public:
+	Particle(){
+		move=Movement();
+		mass=100;
+	};
+
+	Particle(Movement _move,int _mass){
+		this->move=_move;
+		this->mass=_mass;
+	};
+
+	Movement getMovementData(){
+		return this->move;
+	};
+};
