26eb02c838387fa797a15cbe4f60d1330229b5d3
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-01-10 19:12:56 +0530

    Physics class basic skeleton.

---
+++ physics.hpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "particle.hpp"
+
+class physics
+{
+public:
+	physics();
+
+	int getSideToCollideWith(Movement move){
+		//TODO
+	}
+
+	Movement changedMovement(Movement move){
+		//TODO
+	}
+};
