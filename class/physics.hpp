814e4b7d2170c8d76dce66a704aed094f9b6734b
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-01-11 23:39:23 +0530

    Initial veersion of P2P collision.

--- class/physics.hpp
+++ class/physics.hpp
@@ -19,20 +19,54 @@ public:
 	}
 
 	//Gets the time of collision of the particle with the wall.
-	int getTimeOfCollisionP2P(Movement movA,
+	double getTimeOfCollisionP2P(Movement movA,
 		Movement movB,
 		std::tuple<int,int> cordA,
-		std::tuple<int,int> cordB){
-		//TODO
+		std::tuple<int,int> cordB,
+		int radiusA,
+		int radiusB){
+		std::tuple<double,double> velA=movA.getVelocity();
+		std::tuple<double,double> velB=movB.getVelocity();
+		bool xCorSame=isSameI(std::get<0>(cordA),std::get<0>(cordB));
+		bool yCorSame=isSameI(std::get<1>(cordA),std::get<1>(cordB));
+		bool xVelSame=isSameD(std::get<0>(velA),std::get<0>(velB));
+		bool yVelSame=isSameD(std::get<1>(velA),std::get<1>(velB));
+		if(xCorSame && yCorSame){
+			return 0.0;
+		}
+		else if(xCorSame && !yCorSame){
+			double timeY=(std::get<1>(cordA)-std::get<1>(cordB))/(std::get<1>(velA)-std::get<1>(velB));
+			if(xVelSame){
+				if(timeY>=0){
+					return timeY;
+				}
+			}
+		}
+		else if(!xCorSame && yCorSame){
+			double timeX=(std::get<0>(cordA)-std::get<0>(cordB))/(std::get<0>(velA)-std::get<0>(velB));
+			if(yVelSame){
+				if(timeX>=0){
+					return timeX;
+				}
+			}
+		}
+		else if(!xCorSame && !yCorSame){
+			double timeX=(std::get<0>(cordA)-std::get<0>(cordB))/(std::get<0>(velA)-std::get<0>(velB));
+			double timeY=(std::get<1>(cordA)-std::get<1>(cordB))/(std::get<1>(velA)-std::get<1>(velB));
+			if(isSameD(timeX,timeY) && timeX>=0.0){
+				return timeX;
+			}
+		}
+		return -1.0;
 	}
 
 	//Gets the time of collision of the two particles.
-	int getTimeOfCollisionP2B(Movement movA,set::tuple<int,int> corA){
+	int getTimeOfCollisionP2B(Movement movA,std::tuple<int,int> corA){
 		//TODO
 	}
 
 	//Gets the finalVelocities of the particles after the collision.
-	pair<Movement,Movement> getFinalVelocities(Particle particleA,Particle particleB){
+	std::pair<Movement,Movement> getFinalVelocities(Particle particleA,Particle particleB){
 		Movement moveA=particleA.getMovementData();
 		Movement moveB=particleB.getMovementData();
 		int massA=particleA.getMass();
@@ -43,7 +77,7 @@ public:
 			tO.aT(tO.sM(velA,(massA-massB)),tO.sM(velB,2*massA)),
 			massA+massB);
 		std::tuple<double,double> finalVelB=tO.dT(
-			tO.aT(tO.sM(velB,(massB-massA)),tO.sM(valA,2*massB)),
+			tO.aT(tO.sM(velB,(massB-massA)),tO.sM(velA,2*massB)),
 			massA+massB);
 		moveA.setVelocity(finalVelA);
 		moveB.setVelocity(finalVelB);
